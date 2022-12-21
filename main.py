7f49240564055d5cdf688df9596db3f527a1bc26
blackhawk707070 <atounsi@stevens.edu>
2022-12-21 11:08:08 -0500

    prepare code for submission

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import multiprocessing
 import os
-import time
-from pprint import pprint
+
 import hydroeval as he
 import pandas as pd
 from keras.layers import Dense, LSTM, Dropout
@@ -12,12 +11,11 @@ from pandas import DataFrame, concat
 from sklearn.metrics import mean_absolute_error, r2_score
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import MinMaxScaler
-from tqdm import tqdm
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def add_steps(mrms: DataFrame, n_steps: int,_col='Q'):
+def add_steps(mrms: DataFrame, n_steps: int, _col='Q'):
     # split a univariate sequence into samples
     sequence = mrms
     sequence = list(sequence[_col])
@@ -30,12 +28,11 @@ def add_steps(mrms: DataFrame, n_steps: int,_col='Q'):
         seq_x = sequence[i:end_ix]
         X.append(seq_x)
     X = DataFrame(X)
-    X.columns = [_col+str(e) for e in X.columns]
+    X.columns = [_col + str(e) for e in X.columns]
     return X
 
 
-
-def load_data(_filename, n_steps = 4):
+def load_data(_filename, n_steps=4, without=False):
     data = pd.read_csv(os.path.join(ROOT_DIR, 'MOPEX_TS_Ach', _filename), date_parser=['datetime'],
                        parse_dates=True).set_index('datetime')
     data = data[data['Q'] >= 0]
@@ -44,18 +41,15 @@ def load_data(_filename, n_steps = 4):
     # # split into samples
     mrms = data.iloc[:-n_steps, :].reset_index()
     mrms = concat([X, mrms], axis=1).set_index('datetime')
-    mrms = mrms.drop(['Q0'], axis =1)
+    mrms = mrms.drop(['Q0'], axis=1)
+    if without == True:
+        mrms = mrms.drop(['GF'], axis=1)
     return mrms
 
 
-# def split_data(data):
-#     train_x, test_x, train_y, test_y = train_test_split(data[[e for e in data.columns if e != 'Q']], data['Q'],
-#                                                         train_size=0.7, test_size=0.3, shuffle=False)
-#     return train_x, test_x, train_y, test_y
-
 def split_data(data):
-    train = data.iloc[:int(len(data)*0.8)]
-    test = data.iloc[int(len(data)*0.8):]
+    train = data.iloc[:int(len(data) * 0.8)]
+    test = data.iloc[int(len(data) * 0.8):]
     return train, test
 
 
@@ -79,7 +73,7 @@ def model_architect(data, units: int = 200, drp: float = 0.1):
     # Initialising the RNN
     model = Sequential()
 
-    model.add(LSTM(units=units, return_sequences=True, input_shape=(data.shape[1]-1, 1)))
+    model.add(LSTM(units=units, return_sequences=True, input_shape=(data.shape[1] - 1, 1)))
     model.add(Dropout(drp))
 
     # Adding a second LSTM layer and Dropout layer
@@ -101,7 +95,7 @@ def model_architect(data, units: int = 200, drp: float = 0.1):
     return model
 
 
-def compiler(model, x_train, y_train):  # , x_validate, y_validate):
+def compiler(model, x_train, y_train, x_validate, y_validate):
     try:
         cpus = multiprocessing.cpu_count()
     except NotImplementedError:
@@ -114,7 +108,7 @@ def compiler(model, x_train, y_train):  # , x_validate, y_validate):
         y_train,
         epochs=100,
         batch_size=256,
-        # validation_data=(x_validate, y_validate),
+        validation_data=(x_validate, y_validate),
         workers=cpus,
         use_multiprocessing=True,
         verbose=2,
@@ -123,7 +117,7 @@ def compiler(model, x_train, y_train):  # , x_validate, y_validate):
     return model, history
 
 
-def modeler(train):
+def modeler(train, validate):
     """
     Compute the model architecture
     :param train: train data
@@ -133,13 +127,13 @@ def modeler(train):
 
     # scale the train and test
     x_train, y_train, scaler = scale_data(train)
-    # x_validate, y_validate, _scaler = scale_data(validate)
+    x_validate, y_validate, _scaler = scale_data(validate)
 
     # Initialize the model architecture
     model = model_architect(train, units=200, drp=0.1)
 
     # Train the model
-    model, history = compiler(model, x_train, y_train)  # , x_validate, y_validate)
+    model, history = compiler(model, x_train, y_train, validate, x_validate, y_validate)
 
     return x_train, y_train, model, history
 
@@ -167,10 +161,10 @@ def predictor(test, model):
     return x_test, y_test, y_pred, test
 
 
-def statistics(_time, y_test, y_pred):
+def statistics(station_name, y_test, y_pred):
     """
     Compute the statistics of the model
-    :param _time: the lead time used for the data
+    :param station_name: the station_name used for the data
     :param n_steps: number of steps used for the data
     :param y_test: true y data
     :param y_pred: predicted y data
@@ -179,41 +173,17 @@ def statistics(_time, y_test, y_pred):
     kge, r, alpha, beta = he.evaluator(he.kge, y_pred, y_test)
     pbias_ = he.evaluator(he.pbias, y_pred, y_test)
     data = {
-        'station': _filename.split('.')[0],
+        'station': station_name.split('.')[0],
         'MAE': mean_absolute_error(y_test, y_pred),
         'R_squared': r2_score(y_test, y_pred),
         'MSE_squared': mean_squared_error(y_test, y_pred),
         'RMSE': mean_squared_error(y_test, y_pred, squared=True),
         'NSE': he.evaluator(he.nse, y_pred, y_test)[0],
         'KGE': kge[0],
-        "PBias" : pbias_[0],
+        "PBias": pbias_[0],
         'r': r[0],
         'Alpha': alpha[0],
         'Beta': beta[0]
     }
 
     return data
-
-ress = pd.read_csv(os.path.join(ROOT_DIR,'output.csv'))
-ress['station'] = ress['station'].apply(lambda x: x.split('/')[-1])
-res = []
-errors = []
-for _filename in tqdm([os.path.join(ROOT_DIR, 'MOPEX_TS_GF_Ach', e) for e in os.listdir(os.path.join(ROOT_DIR,'MOPEX_TS_GF_Ach'))]):
-    if _filename.split('/')[-1].split('.')[0] not in list(ress['station']):
-        try:
-            start = time.time()
-            # _filename ='1048000.csv'
-            data = load_data(_filename)
-            train, test = split_data(data)
-            x_train, y_train, model, history = modeler(train)
-            x_test, y_test, y_pred, test = predictor(test, model)
-
-            stats = statistics(_filename, test['Q'],test['pred_Q'])
-            print(" Time taken for station ", time.time() - start)
-            res.append(stats)
-
-            DataFrame(res).to_csv(os.path.join(ROOT_DIR,'output.csv'))
-        except:
-            print("Error with station ", _filename.split('/')[-1])
-            errors.append(_filename.split('/')[-1])
-            pass
